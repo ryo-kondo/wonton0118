877d92902c9e6feb90fd46acf61d689dab7c863b
ryo-kondo <wonton0118@gmail.com>
2018-12-29 12:29:15 +0900

    initial commit

---
+++ web/static/js/entry.jsx
@@ -0,0 +1,28 @@
+import 'react-hot-loader/patch';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import Root from './app/Root';
+
+const rootEl = document.getElementById('root');
+console.log(rootEl);
+
+ReactDOM.render(
+  <AppContainer>
+    <Root />
+  </AppContainer>,
+  rootEl
+);
+
+if (module.hot) {
+  module.hot.accept('./app/Root', () => {
+    const NextRoot = require('./app/Root').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextRoot />
+      </AppContainer>,
+      rootEl
+    );
+  });
+}
